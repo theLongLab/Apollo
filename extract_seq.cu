11a655a9e43205b133fa51a5fd606895060c57a4
DeshanPerera <desh.02236@gmail.com>
2023-07-31 22:11:35 -0600

    extractor update

--- extract_seq.cu
+++ extract_seq.cu
@@ -40,6 +40,11 @@ void extract_seq::ingress()
 
     int num_Generations = 0;
 
+    fstream sequences_files_Summary;
+    sequences_files_Summary.open(output_Folder_Sequences + "/sequences_Summary.csv", ios::out);
+    sequences_files_Summary << "First_occurrence\tSequence\tCount\n";
+    sequences_files_Summary.close();
+
     cout << "Intermediate sequences folder: " << this->intermediate_sequence_Store << endl;
 
     for (const auto &entry : filesystem::directory_iterator(intermediate_sequence_Store))
@@ -82,7 +87,127 @@ void extract_seq::ingress()
 
                     // cout << "Processing sequence: " << file_Name << endl;
                 }
-                cout << sequence_Files_count << " sequence(s) found." << endl;
+                cout << sequence_Files_count << " sequence(s) found.\n"
+                     << endl;
+
+                if (multi_READ == "YES")
+                {
+                    cout << "Intiating multi read of N_sequences.\n";
+
+                    int full_Rounds = sequence_Files_count / this->gpu_Limit;
+                    int partial_Rounds = sequence_Files_count % this->gpu_Limit;
+
+                    vector<pair<int, int>> start_stops;
+
+                    for (int full = 0; full < full_Rounds; full++)
+                    {
+                        int start = full * this->gpu_Limit;
+                        int stop = start + this->gpu_Limit;
+                        start_stops.push_back(make_pair(start, stop));
+                    }
+
+                    if (partial_Rounds != 0)
+                    {
+                        int start = sequence_Files_count - partial_Rounds;
+                        start_stops.push_back(make_pair(start, sequence_Files_count));
+                    }
+
+                    for (size_t i = 0; i < start_stops.size(); i++)
+                    {
+                        vector<thread> threads_vec;
+                        int num_of_values_current = start_stops[i].second - start_stops[i].first;
+                        int num_per_Core = num_of_values_current / this->CPU_cores;
+                        int remainder = num_of_values_current % this->CPU_cores;
+
+                        for (int sequence = 0; sequence < num_of_values_current; sequence++)
+                        {
+                            N_sequences_Store.push_back("");
+                        }
+
+                        for (int core_ID = 0; core_ID < this->CPU_cores; core_ID++)
+                        {
+                            int start_Cell = core_ID * num_per_Core;
+                            int stop_Cell = start_Cell + num_per_Core;
+
+                            threads_vec.push_back(thread{&extract_seq::read_N_fasta, this, start_Cell, stop_Cell, start_stops[i].first, folder_Index, folder});
+                        }
+
+                        if (remainder != 0)
+                        {
+                            int start_Cell = num_of_values_current - remainder;
+                            int stop_Cell = num_of_values_current;
+
+                            threads_vec.push_back(thread{&extract_seq::read_N_fasta, this, start_Cell, stop_Cell, start_stops[i].first, folder_Index, folder});
+                        }
+
+                        for (thread &t : threads_vec)
+                        {
+                            if (t.joinable())
+                            {
+                                t.join();
+                            }
+                        }
+
+                        threads_vec.clear();
+
+                        cout << endl;
+
+                        // for (size_t i = 0; i < N_sequences_Store.size(); i++)
+                        // {
+                        //     cout << N_sequences_Store[i] << endl
+                        //          << endl;
+                        // }
+
+                        for (int query = 0; query < N_sequences_Store.size(); query++)
+                        {
+                            int found = 0;
+
+                            for (int check = 0; check < unique_Sequences.size(); check++)
+                            {
+                                if (N_sequences_Store[query] == unique_Sequences[check].second)
+                                {
+                                    sequence_Count[check] = sequence_Count[check] + 1;
+                                    found = 1;
+                                    break;
+                                }
+                            }
+
+                            if (found == 0)
+                            {
+                                string sequence_ID = to_string(folder_Index) + "_" + to_string(query + start_stops[i].first);
+
+                                unique_Sequences.push_back(make_pair(sequence_ID, N_sequences_Store[query]));
+                                sequence_Count.push_back(1);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    cout << "Intiating single read of N_sequences.\n";
+                    for (int sequence = 0; sequence < sequence_Files_count; sequence++)
+                    {
+                        string sequence_Name = to_string(folder_Index) + "_" + to_string(sequence) + ".nfasta";
+                        cout << "Processing sequence : " << folder << "/" << sequence_Name;
+
+                        fstream sequence_File;
+                        sequence_File.open(folder + "/" + sequence_Name, ios::in);
+
+                        string sequence_Full = "";
+
+                        if (sequence_File.is_open())
+                        {
+                            string line;
+
+                            while (getline(sequence_File, line))
+                            {
+                                sequence_Full.append(line);
+                            }
+                            sequence_File.close();
+                            N_sequences_Store.push_back(sequence_Full);
+                        }
+                    }
+                }
             }
             else
             {
@@ -91,9 +216,83 @@ void extract_seq::ingress()
             }
             break;
         }
+
+        cout << "Writing results of sequence summary\n";
+
+        sequences_files_Summary.open(output_Folder_Sequences + "/sequences_Summary.csv", ios::app);
+
+        for (int unique = 0; unique < unique_Sequences.size(); unique++)
+        {
+            sequences_files_Summary << unique_Sequences[unique].first << "\t";
+
+            for (int base = 0; base < unique_Sequences[unique].second.size(); base++)
+            {
+                if (unique_Sequences[unique].second.at(base) == '0')
+                {
+                    sequences_files_Summary << "A";
+                }
+                else if (unique_Sequences[unique].second.at(base) == '1')
+                {
+                    sequences_files_Summary << "T";
+                }
+                else if (unique_Sequences[unique].second.at(base) == '2')
+                {
+                    sequences_files_Summary << "G";
+                }
+                else if (unique_Sequences[unique].second.at(base) == '3')
+                {
+                    sequences_files_Summary << "C";
+                }
+            }
+
+            sequences_files_Summary << "\t";
+
+            sequences_files_Summary << sequence_Count[unique] << "\n";
+        }
+
+        sequences_files_Summary.close();
     }
     else
     {
         cout << "No generational sequence data was found.\n";
+    }
+}
+
+void extract_seq::read_N_fasta(int start, int stop, int start_Index, int generation, string folder)
+{
+    vector<string> N_sequences_Store_partial;
+
+    // cout << "check\n";
+
+    for (int i = start; i < stop; i++)
+    {
+        int sequence = i + start_Index;
+
+        string sequence_Name = to_string(generation) + "_" + to_string(sequence) + ".nfasta";
+
+        cout << "Processing sequence : " << folder << "/" << sequence_Name << endl;
+
+        fstream sequence_File;
+        sequence_File.open(folder + "/" + sequence_Name, ios::in);
+
+        string sequence_Full = "";
+
+        if (sequence_File.is_open())
+        {
+            string line;
+
+            while (getline(sequence_File, line))
+            {
+                sequence_Full.append(line);
+            }
+            sequence_File.close();
+            N_sequences_Store_partial.push_back(sequence_Full);
+        }
+    }
+
+    unique_lock<shared_mutex> ul(g_mutex);
+    for (int i = start; i < stop; i++)
+    {
+        N_sequences_Store[i] = N_sequences_Store_partial[i - start];
     }
 }
--- extract_seq.cuh
+++ extract_seq.cuh
@@ -36,6 +36,9 @@ class extract_seq
 private:
     mt19937 gen;
 
+    shared_mutex g_mutex;
+    mutex gi_mutex;
+
     int CUDA_device_number;
     int gpu_Limit;
     int tot_Blocks;
@@ -49,6 +52,11 @@ private:
 
     string multi_READ = "NO";
 
+    vector<string> N_sequences_Store;
+
+    vector<pair<string, string>> unique_Sequences;
+    vector<int> sequence_Count;
+
 public:
     extract_seq(int CUDA_device_number, int CPU_cores, int gpu_Limit,
                 string intermediate_Folders,
@@ -56,4 +64,6 @@ public:
                 string multi_READ);
 
     void ingress();
+
+    void read_N_fasta(int start, int stop, int start_Index, int generation, string folder);
 };
